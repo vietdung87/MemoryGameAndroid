95370d72e9114006b78453f460f67f1400fd8ea4
vietdung87 <vietdung5787@gamil.com>
2013-12-03 09:50:27 +0200

    first commit

---
+++ src/fi/metropolia/vietn/MainActivity.java
@@ -0,0 +1,21 @@
+package fi.metropolia.vietn;
+
+import android.os.Bundle;
+
+
+import org.apache.cordova.DroidGap;
+
+
+public class MainActivity  extends DroidGap  {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+
+    }
+
+
+   
+    
+}
